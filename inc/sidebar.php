a48560d6e4c38eafe975ae7bf349312f855441f0
AaronFagan <AaronFagan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-07 18:53:27 +0000

    New version.
    git-svn-id: https://plugins.svn.wordpress.org/rad-text-highlighter/tags/1.0.1@677602 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/sidebar.php
@@ -0,0 +1,27 @@
+<div class="meta-box-sortables">
+<div id="about" class="postbox">
+<h3 class="hndle" id="about-sidebar">Connect With Us!</h3>
+<div class="inside">
+<p>
+<a href="http://www.facebook.com/afIndustriesInc" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/facebook.png "></a>&nbsp;
+<a href="http://www.twitter.com/intent/user?screen_name=afIndustries" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/twitter.png "></a>&nbsp;
+<a href="http://www.afwebdesign.ca/blog" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/wordpress.png "></a>&nbsp;
+<a href="http://www.afwebdesign.ca/feed" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/rss.png "></a>&nbsp;
+<a href="http://www.afwebdesign.ca/contact" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/email.png "></a>
+</p>
+</div>
+</div>
+</div>
+
+<div class="meta-box-sortables">
+<div id="about" class="postbox">
+<h3 class="hndle" id="about-sidebar">Resources & Information</h3>
+<div class="inside">
+<p>
+<li><a href="http://www.afwebdesign.ca/blog/2013/wordpress-plugin-rad-text-highlighter/" target="_blank">Plugin Homepage</a></li>
+<!-- <li><a href="http://docs.radmediaco.com/rad-text-highlighter" target="_blank">Documentation</a></li> -->
+<li><a href="http://www.afwebdesign.ca/" target="_blank">af Web Design</a></li>
+</p>
+</div>
+</div>
+</div>
