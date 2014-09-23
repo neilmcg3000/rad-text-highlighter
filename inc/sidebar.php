cca20374f0e75964026646d9f71762abfbe7637a
Neil McGillivray <nmcgillivray@mac.com>
2014-09-23 11:06:31 +0200

    Update sidebar.php

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -3,11 +3,11 @@
 <h3 class="hndle" id="about-sidebar">Connect With Us!</h3>
 <div class="inside">
 <p>
-//<a href="http://www.facebook.com/afIndustries" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/facebook.png "></a>&nbsp;
-//<a href="http://www.twitter.com/intent/user?screen_name=afIndustries" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/twitter.png "></a>&nbsp;
-//<a href="http://www.afcreative.ca/blog" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/wordpress.png "></a>&nbsp;
-//<a href="http://www.afcreative.ca/feed" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/rss.png "></a>&nbsp;
-//<a href="http://www.afcreative.ca/contact" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/email.png "></a>
+//<a href="http://www.facebook.com/" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/facebook.png "></a>&nbsp;
+//<a href="http://www.twitter.com/intent/user?screen_name=" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/twitter.png "></a>&nbsp;
+//<a href="/blog" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/wordpress.png "></a>&nbsp;
+//<a href="/feed" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/rss.png "></a>&nbsp;
+//<a href="/contact" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-text-highlighter/assets/email.png "></a>
 </p>
 </div>
 </div>
@@ -19,7 +19,7 @@
 <div class="inside">
 <p>
 <li><a href="http://wordpress.org/plugins/rad-text-highlighter/" target="_blank">Plugin Homepage</a></li>
-<li><a href="http://www.afcreative.ca/" target="_blank">af Creative</a></li>
+<li><a href="http://" target="_blank">af Creative</a></li>
 </p>
 </div>
 </div>
